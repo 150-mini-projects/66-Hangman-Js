964de8318312ac201ac0140e163042f19d781d01
interactivepix <122866945+interactivepix@users.noreply.github.com>
2023-09-02 18:59:37 +0100

    Commit

---
+++ scripts/word-list.js
@@ -0,0 +1,36 @@
+const wordList = [
+  {
+    word: "Accelerator ",
+    hint: "A machine which accelerates charged particles to high energies.",
+  },
+
+  {
+    word: "Electromagnet",
+    hint: "A wire coil around a metal core (usually iron) that acts like a magnet when an electric current flows through it.",
+  },
+
+  {
+    word: "Matter",
+    hint: "Something that has mass which can exist in the form of a solid, liquid, gas or plasma.",
+  },
+
+  {
+    word: "Laboratory",
+    hint: "A place equipped for scientific research, experiments or testing.",
+  },
+
+  {
+    word: "Liquid",
+    hint: "A state of matter with definite volume but no definite shape, like water.",
+  },
+
+  {
+    word: "Hypothesis",
+    hint: "An educated guess that can be tested or investigated.",
+  },
+
+  {
+    word: "Fahrenheit",
+    hint: "A temperature scale at which water freezes at 32° and boils at 212°.",
+  },
+];
